6074a4afce8ecc23eae311853180318cdbdd687e
SaulTereso <al221910372@gmail.com>
2022-08-14 18:02:54 -0500

    Estado del componente y Vue DevTools

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,10 +1,17 @@
 <template>
     <h1>Primer Componente - Counter</h1>
+    <h4>Operacion matématica exponencial</h4>
+    <p>{{ counter }} <sup>2</sup> = {{ counter * counter }}</p>
 </template>
 
 <script>
 export default {
-    name: 'PrimerComponente'
+    data(){
+        return{
+            name: 'PrimerComponente',
+            counter: 5
+        }
+    }
 }
 </script>
 
