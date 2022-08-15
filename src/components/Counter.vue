ea872b902363ad848c8fb78fabe7efd8e62e4eb2
SaulTereso <al221910372@gmail.com>
2022-08-14 19:22:10 -0500

    Propiedades computadas

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,6 +2,8 @@
     <h1>Primer Componente - Counter</h1>
     <h4>Operacion matématica exponencial</h4>
     <p>{{ counter }} <sup>2</sup> = {{ counter * counter }}</p>
+    <p>{{ counter }} <sup>2</sup> = {{ getSquareValue() }}</p>
+    <p>{{ counter }} <sup>2</sup> = {{ squareCounter }}</p>
 </template>
 
 <script>
@@ -10,6 +12,18 @@ export default {
         return{
             name: 'PrimerComponente',
             counter: 5
+        }
+    },
+    //Otras formas de hacer la operacion y tener el mismo resultado
+    methods:{
+        getSquareValue(){
+            return this.counter * this.counter
+        }
+    },
+    //Propiedades computadas - Computed Properties
+    computed:{
+        squareCounter(){
+            return this.counter * this.counter
         }
     }
 }
